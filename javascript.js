b4183ccc17d74ba37a3230f1c2d3e3d0b5c0b4b0
137725897+chadzimmerman@users.noreply.github.com <bookingpedestals@gmail.com>
2025-07-07 16:24:07 +0300

    Updated clock, career section and certificates. Must adjust hero__profile later.

--- javascript.js
+++ javascript.js
@@ -5,3 +5,13 @@ document.querySelector('a[href="#about"]').addEventListener('click', function (e
         block: 'start',
     });
 });
+
+function updateClock() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('clock').textContent = timeString;
+  }
+  
+  // Update clock immediately and every second
+  updateClock();
+  setInterval(updateClock, 1000);
